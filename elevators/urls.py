4e8cc634f8022375b9798e39d26c9252fad8b93b
Arpit <arpitjain310@gmail.com>
2024-01-05 02:37:20 +0530

    updated elevator initialization logic

--- elevators/urls.py
+++ elevators/urls.py
@@ -7,11 +7,12 @@ router.register(r'elevators', ElevatorViewSet, basename='elevator')
 
 elevator_system_router = DefaultRouter()
 elevator_system_router.register(
-    r'elevators', ElevatorSystemViewSet, basename='elevator-system')
+    r'elevator-system', ElevatorSystemViewSet, basename='elevator-system')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('elevator-system/', ElevatorSystemViewSet.as_view({'get': 'requests'})),
+    path('', include(elevator_system_router.urls)),
+    path('elevator-system/', ElevatorSystemViewSet.as_view({'post': 'initialize'})),
     path('elevators/<int:pk>/requests/',
          ElevatorViewSet.as_view({'get': 'requests'}), name='elevator-requests'),
     path('elevators/<int:pk>/next_destination/', ElevatorViewSet.as_view(
